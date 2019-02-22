2a365cd72c45b0597ddb1cc6e2c37dd3f4af079b
Stephen Fowles-White <FowlesWhite@gmail.com>
2019-02-22 08:01:54 +0000

    Started rendering img
    cemented learning for rendering Elements to the DOM

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-const description = 'I just learnt how to create a React node and render it to the DOM ';
-const myTitleId = 'main-title';
-const name = 'Guil';
 
 
-const header =
-    ( <header>
-        <h1 id = { myTitleId } > { name }'s First React Element' </h1>
-        <p> { description } </p> 
-        </header>
-    );
-
-
-
+const openImage = 
+(
+    <div id ='mainImg'>
+     <img src="/images/OpenBackground.jpg" alt= "This is the main" />
+    </div>
+);
 
 ReactDOM.render(
-    header,
+    openImage,
     document.getElementById('root'));
+
